af63d55238b70012bb5bc879d092c8a15ea6cfb8
Carlos Roque <carlosroque198@gmail.com>
2021-06-21 10:21:59 -0400

    Second test passed

--- IsomorphicStringsProject/isotests.c
+++ IsomorphicStringsProject/isotests.c
@@ -6,6 +6,11 @@ spec("This program tests if given strings are isomorphic")
 {
 	it("Should return true since two strings with whitespace are isomorphic")
 	{
-		check(areIsomorphic(" "," ") == true);
+		check(areIsomorphic(" ", " ") == true);
+	}
+	
+	it("Should return true since egg and add are isomorphic")
+	{
+		check(areIsomorphic("egg", "add") == true);
 	}
 }
